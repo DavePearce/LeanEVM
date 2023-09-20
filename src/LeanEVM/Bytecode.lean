e73ae10a1085f64a1b1d1c73b2685c099bb180e6
DavePearce <dave01001110@gmail.com>
2023-09-20 14:04:05 +1200

    Update semantics of `ADD`
    This improves the semantics of the `ADD` bytecode such that it now pops
    both operands from the stack.  Unfortunately, getting `EvmStack.pop` to
    verify was a major headache :(

--- src/LeanEVM/Bytecode.lean
+++ src/LeanEVM/Bytecode.lean
@@ -1,37 +1,56 @@
 import LeanEVM.State
 import LeanEVM.Util
-import Mathlib.Tactic.Linarith
 
 open Bytecode
 open Exception
 open Outcome
 
-def ZERO : u256 := Fin.ofNat 0
+-- ==================================================================
+-- 0s: Stop and Arithmetic Operations
+-- ==================================================================
 
-def eval_stop (_: Evm) : Outcome :=
+-- Halt the machine without any return data
+def STOP (_: Evm) : Outcome :=
   Done 0 Array.empty
 
-def eval_add (evm: Evm) : Outcome :=
+-- Unsigned integer addition with modulo arithmetic
+def ADD (evm: Evm) : Outcome :=
   if r:evm.stack.length > 1
   then
-    let lhs : u256 := evm.peek 0 (by sorry);
+    let lhs : u256 := evm.peek 0 (by linarith);
     let rhs : u256 := evm.peek 1 (by simp [r]);
-    let res : u256 := lhs.add rhs
-    Ok (evm.push res)
+    let res : u256 := (u256.add lhs rhs);
+    -- Take operands off stack
+    let evm' := evm.pop 2 r;
+    -- Push result on stack
+    Ok (evm'.push res)
   else
     Error StackUnderflow
 
-def eval_push (evm: Evm)(n:u256) : Outcome :=
-  Ok (evm.push n)
+-- ==================================================================
+-- 50s: Stack, Memory Storage and Flow Operations
+-- ==================================================================
 
-def eval_pop (evm: Evm) : Outcome :=
+-- Pop word from stack
+def POP (evm: Evm) : Outcome :=
   if r:evm.stack.length > 0
   then
-    Ok (evm.pop r)
+    Ok (evm.pop 1 r)
   else
     Error StackUnderflow
 
-def eval_dup (evm: Evm) : Outcome :=
+-- ==================================================================
+-- 60s & 70s: Push Operations
+-- ==================================================================
+
+def PUSH (evm: Evm)(n:u256) : Outcome :=
+  Ok (evm.push n)
+
+-- ==================================================================
+-- 80s: Duplication Operations
+-- ==================================================================
+
+def DUP (evm: Evm) : Outcome :=
   if r:evm.stack.length > 0
   then
     let val : u256 := evm.peek 0 (by simp [r]);
@@ -39,13 +58,17 @@ def eval_dup (evm: Evm) : Outcome :=
   else
     Error StackUnderflow
 
+-- ==================================================================
+-- Eval(uate)
+-- ==================================================================
+
 -- Execute a given bytecodes from a given state.
 @[simp] def eval (evm: Evm) : Bytecode -> Outcome
-| Bytecode.STOP => eval_stop evm
-| Bytecode.ADD => eval_add evm
-| Bytecode.POP => eval_pop evm
-| Bytecode.DUP => eval_dup evm
-| Bytecode.PUSH n => eval_push evm n
+| Bytecode.Stop => STOP evm
+| Bytecode.Add => ADD evm
+| Bytecode.Pop => POP evm
+| Bytecode.Dup => DUP evm
+| Bytecode.Push n => PUSH evm n
 
 -- Execute a sequence of zero or more bytecodes from a given state.
 @[simp] def eval_all (code: List Bytecode)(evm:Evm) : Outcome :=
@@ -62,35 +85,35 @@ where
 /- =============================================================== -/
 
 -- Executing STOP on an arbitrary Evm produces Done.
-example (evm:Evm) : (eval_all [STOP] evm) = (Done 0 Array.empty) :=
+example (evm:Evm) : (eval_all [Stop] evm) = (Done 0 Array.empty) :=
 by
   rfl
 
 -- Executing ADD on an empty Evm produces a stack overflow.
-example : (eval_all [ADD] EmptyEvm) = (Error StackUnderflow) :=
+example : (eval_all [Add] EmptyEvm) = (Error StackUnderflow) :=
 by
   simp
 
 -- Executing ADD on Evm with two operands succeeds.
-example (st:List u256): exists evm, (eval_all [ADD] {stack:=l::r::st}) = (Ok evm) :=
+example (st:List u256): exists evm, (eval_all [Add] {stack:=l::r::st}) = (Ok evm) :=
 by
   exists {stack := (Fin.add l r)::st}
   sorry
 
 -- Executing POP on Evm with at least one operand succeeds.
-example (st:List u256)(p:st = v::rest): (eval_all [POP] {stack:=st}) = (Ok {stack:=rest}) :=
+example (st:List u256)(p:st = v::rest): (eval_all [Pop] {stack:=st}) = (Ok {stack:=rest}) :=
 by
   simp [p]
   sorry
 
 -- Executing PUSH then POP on an Evm is a no-op.
-example (evm:Evm): (eval_all [PUSH n,POP] evm) = Ok evm :=
+example (evm:Evm): (eval_all [Push n,Pop] evm) = Ok evm :=
 by
-  simp [Evm.push, EvmStack.push, eval_all.reduce, eval_pop]
+  simp [Evm.push, EvmStack.push, eval_all.reduce, POP]
   sorry
 
 -- Executing PUSH, PUSH, ADD on an Evm always succeeds.
-example (evm:Evm): ∃nevm, (eval_all [PUSH N, PUSH m, ADD] evm) = Ok nevm :=
+example (evm:Evm): ∃nevm, (eval_all [Push N, Push m, Add] evm) = Ok nevm :=
 by
   simp
   sorry
