8792324dc41ca69a981534e8db2cf81ab178bed0
DavePearce <dave01001110@gmail.com>
2024-03-28 10:39:41 +1300

    Working on `Bytes.from_bytes_be`

--- src/LeanEVM/Byte.lean
+++ src/LeanEVM/Byte.lean
@@ -8,24 +8,17 @@ opaque BYTE_0 : byte := {val:=0, isLt:=(by simp)}
 -- Construct a natural number from a sequence of one or more bytes store in big
 -- endian form.
 def from_bytes_be(bytes:List byte)(i: Fin bytes.length) : Nat :=
-  -- Read ith byte
-  let b : byte := bytes[i];
-  -- Decide what to do
-  if r:i.val = 0 then b.val
-  else
-    -- Construct i-1
-    let im1 : Fin bytes.length := {val:=i.val-1,isLt:=(
-      by
-        have p : i.val-1 < i.val := (Nat.pred_lt r);
-        linarith [i.isLt])
-    };
-    -- Done
-    (256 * (from_bytes_be bytes im1)) + b.val
+  match bytes, p:i.val with
+  | b::_, 0 => b.val
+  | b::bs, k+1 =>
+      have q0 : i.val = k+1 := by sorry -- should be easy??
+      have q1 : k < bs.length := by sorry
+      let im1 : Fin bs.length := {val:= k, isLt:=by exact q1}
+      (256 * (from_bytes_be bs im1)) + b.val
 
 -- Prove that converting an array consisting of a single byte generates the
 -- corresponding Nat.
 example (n:byte)(i:Fin 1): (from_bytes_be [n] i) = n.val :=
 by
-  unfold from_bytes_be
   match i with
-  | 0 => simp [*]; rfl
+  | 0 => rfl
