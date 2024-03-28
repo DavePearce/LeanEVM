b7f8c859bb9dc269a4d589042cf78e9f16d583b7
DavePearce <dave01001110@gmail.com>
2024-03-28 13:13:00 +1300

    Fixed proof for `from_bytes_be`
    Actually, however, the whole thing is a little broken.  There are a few
    things we could do:
    
    * For a `Push bs` instruction, restrict length of the bytes to max 32.
    
    * Then change `from_bytes_be` to just read all bytes and multiply them
      out.  Need a lemma relating size of integer at the end of this all.

--- src/LeanEVM/Byte.lean
+++ src/LeanEVM/Byte.lean
@@ -8,12 +8,11 @@ opaque BYTE_0 : byte := {val:=0, isLt:=(by simp)}
 -- Construct a natural number from a sequence of one or more bytes store in big
 -- endian form.
 def from_bytes_be(bytes:List byte)(i: Fin bytes.length) : Nat :=
-  match bytes, p:i.val with
-  | b::_, 0 => b.val
-  | b::bs, k+1 =>
-      have q0 : i.val = k+1 := by sorry -- should be easy??
-      have q1 : k < bs.length := by sorry
-      let im1 : Fin bs.length := {val:= k, isLt:=by exact q1}
+  match bytes, i with
+  | b::_, {val:=0,isLt:=_} => b.val
+  | b::bs, {val:=k+1,isLt:=p} =>
+      have q : k < bs.length := by exact Nat.succ_lt_succ_iff.mp p
+      let im1 : Fin bs.length := {val:= k, isLt:=by exact q}
       (256 * (from_bytes_be bs im1)) + b.val
 
 -- Prove that converting an array consisting of a single byte generates the
